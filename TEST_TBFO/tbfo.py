ff8767635dcc2fc34c200fbcd2f6f017c11f05ce
GHOST <ghost@localhost.localdomain>
2021-10-16 04:39:40 +0700

    TBFO

--- TEST_TBFO/tbfo.py
+++ TEST_TBFO/tbfo.py
@@ -1,13 +1,37 @@
 class MesinGabut():
     def __init__(self, biner):
         self.biner = biner
+        self.role = ['q1', 'q2', 'q3']
+        self.curr = self.role[0]
+        self.masuk = False
 
-    def cek(self):
-        if self.biner[0] == '0' or self.biner[0] == '1':
-            for i in range(len(self.biner)):
-                print(self.biner[i])
+    def circle(self, binchar):
+        if self.curr == self.role[0]:
+            if binchar == '0':
+                self.curr = self.role[1]
+                self.masuk = False
+            if binchar == '1':
+                self.curr = self.role[2]
+                self.masuk = True
+            
+        if self.curr == self.role[1]:
+            if binchar == '0' or binchar == '1':
+                pass
+
+    def cek(self, curr, jum):
+        if curr < jum:
+            if self.biner[curr] != '0' or self.biner != '1':
+                return False
+            else:
+                return self.cek(curr+1, jum)
+        else:
+            return True
 
 if __name__ == '__main__':
     ambil = input()
-    mulai = MesinGabut(ambil)
-    mulai.cek()
+    mesin = MesinGabut(ambil)
+
+    if cek(0, len(mesin.biner)):
+        print("Sesuai Kriteria")
+    else:
+        print("Tidak Sesuai Kriteria")
